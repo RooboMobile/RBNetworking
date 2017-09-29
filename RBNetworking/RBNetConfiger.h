f3f0455a7ea0354dca2af6f829712acd6421c19b
baxiang <baxiang@roobo.com>
2017-09-29 11:55:01 +0800

    bug

--- RBNetworking/RBNetConfiger.h
+++ RBNetworking/RBNetConfiger.h
@@ -6,44 +6,27 @@
 //
 
 #import <Foundation/Foundation.h>
+@class RBNetEngine;
 
 @interface RBNetConfiger : NSObject
-///-----------------------------------------------
-/// @name Properties to Assign Values for RBCenter
-///-----------------------------------------------
 
-/**
- The general server address to assign for RBCenter.
- */
 @property (nonatomic, copy, nullable) NSString *generalServer;
 
-/**
- The general parameters to assign for RBCenter.
- */
+
 @property (nonatomic, strong, nullable) NSDictionary<NSString *, id> *generalParameters;
 
-/**
- The general headers to assign for RBCenter.
- */
+
 @property (nonatomic, strong, nullable) NSDictionary<NSString *, NSString *> *generalHeaders;
 
-/**
- The general user info to assign for RBCenter.
- */
+
 @property (nonatomic, strong, nullable) NSDictionary *generalUserInfo;
 
-/**
- The dispatch callback queue to assign for RBCenter.
- */
+
 @property (nonatomic, strong, nullable) dispatch_queue_t callbackQueue;
 
-/**
- The global requests engine to assign for RBCenter.
- */
+
 @property (nonatomic, strong, nullable) RBNetEngine *engine;
 
-/**
- The console log BOOL value to assign for RBCenter.
- */
+
 @property (nonatomic, assign) BOOL consoleLog;
 @end
